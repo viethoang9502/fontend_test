f32ec1d48326516e720d3c27ff21a65c9c3d4b28
viethoang <voviethoang952002@gmail.com>
2024-07-28 14:21:46 +0700

    update

---
+++ src/app/components/game-soccer/game-soccer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-soccer',
+  standalone: true,
+  imports: [],
+  templateUrl: './game-soccer.component.html',
+  styleUrl: './game-soccer.component.scss'
+})
+export class GameSoccerComponent {
+
+}
